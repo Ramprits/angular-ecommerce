6e6efafe081b27a2e363f6954dcedd2338d9e84b
Ramprit Sahani <rampritsahani@protonmail.com>
2019-11-09 16:11:44 +0530

    adding PWA

--- apps/myapp/src/app/app.module.ts
+++ apps/myapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { UiModule } from '@workspace/ui';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UiModule, AppRoutingModule],
+  imports: [BrowserModule, UiModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
